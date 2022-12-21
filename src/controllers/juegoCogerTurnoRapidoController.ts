a97aede12d25e5bc1b660e133fd5cb75de00b143
Pablo <kevin.alca@gmail.com>
2022-12-21 18:19:42 +0100

    CRUD of CogerTurnoRapido

---
+++ src/controllers/juegoCogerTurnoRapidoController.ts
@@ -0,0 +1,75 @@
+import { request, Request, Response } from "express";
+import * as juegoCogerTurnoRapidoService from "../services/juegoCogerTurnoRapidoService";
+import { handleHttp } from "../utils/http.handle";
+
+
+const getAllJuegoCuestionarioRapido = (req: Request, res: Response) => {
+try{
+    const allJuegoCogerTurnoRapido = juegoCogerTurnoRapidoService.getAllJuegoCogerTurnoRapido();
+    res.send({status: "OK", data: allJuegoCogerTurnoRapido});
+    } catch (error:any) {
+        handleHttp(res, "ERROR_GET_ALLJUEGO_COGER_TURNO_RAPIDO", error?.status || 500, error);
+    }
+}
+
+const getJuegoCogerTurnoRapidoPorId = ({params}: Request, res: Response ) => {
+    const {id} = params;
+    if(!id) {
+        handleHttp(res, "Falta parametro :id", 400);
+    }
+
+    try {
+        const juegoCogerTurnoRapido = juegoCogerTurnoRapidoService.getJuegoCogerTurnoRapidoPorId(id);
+        res.send(juegoCogerTurnoRapido);
+    } catch (error: any) {
+        handleHttp(res, error?.message || "ERROR_GET_JUEGO_COGER_TURNO_RAPIDO", error?.status || 500, error);
+    }
+}
+
+const getJuegoCogerTurnoRapidoPorProfesor = ({params}: Request, res:Response) => {
+    const {idProfesor} = params;
+    if(!idProfesor) {
+        handleHttp(res, "Falta parametro :idProfesor", 400);
+    }
+}
+
+const createJuegoCogerTurnoRapido = (req: Request, res: Response) => {
+    const {body} = req;
+    try{
+        const createdJuegoCogerTurnoRapido = juegoCogerTurnoRapidoService.createJuegoCogerTurnoRapido(body);
+        res.status(201).send(createdJuegoCogerTurnoRapido)
+    } catch (error:any) {
+        handleHttp(res, error?.message || "ERROR_CREATE_JUEGO_COGER_TURNO_RAPIDO", error?.status || 500, error);
+    }
+}
+
+const updateJuegoCogerTurnoRapido = (req: Request, res: Response) => {
+    const {body} = req;
+    if(!body) {
+        handleHttp(res, "Falta parametros el el body", 400);
+    }
+
+    try {
+        const updatedJuegoCogerTurnoRapido = juegoCogerTurnoRapidoService.updateJuegoCogerTurnoRapido(body);
+        res.send(updatedJuegoCogerTurnoRapido);
+    } catch(error: any) {
+        handleHttp(res, error?.message || "ERROR_UPDATE_JUEGO_COGER_TURNO_RAPIDO", error?.status || 500, error)
+    }
+}
+
+const deleteJuegoCogerTurnoRapido = ({params}: Request, res: Response) => {
+    const {id} = params;
+    if (!id) {
+        handleHttp(res, "Falta paramtro :id", 400);
+    }
+
+    try {
+        const deletedJuegoCogerTurno = juegoCogerTurnoRapidoService.deleteJuegoCogerTurnoRapidoPorId(id);
+        res.send(deletedJuegoCogerTurno);
+    } catch (error: any) {
+        handleHttp(res, error?.message || "ERROR_DELETE_JUEGO_COGER_TURNO_RAPIDO", error?.status || 500, error);
+    }
+
+}
+
+export {getAllJuegoCuestionarioRapido, getJuegoCogerTurnoRapidoPorId, getJuegoCogerTurnoRapidoPorProfesor, createJuegoCogerTurnoRapido, deleteJuegoCogerTurnoRapido, updateJuegoCogerTurnoRapido}
---
+++ src/database/JuegoCogerTurnoRapido.ts
@@ -0,0 +1,96 @@
+import { JuegoCogerTurnoRapido } from "../interfaces/juegoDeCogerTurnoRapidoInterface";
+const DB = require("./db.json");
+import * as utils from "../utils/utilsDatabase";
+
+const getAllCogerTurnoRapido = () => {
+    return DB.JuegoCogerTurnoRapido;
+}
+
+const getCogerTurnoRapidoPorId = (id: string) => {
+    try {
+        const cogerTurnoRapido = DB.JuegoCogerTurnoRapido.find((juegoCogerTurnoRapido: JuegoCogerTurnoRapido) => 
+        juegoCogerTurnoRapido.id === id) ;
+        if(!cogerTurnoRapido) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar el juegoCogerTurnoRapido con id' ${id}`
+            }
+        }
+        return cogerTurnoRapido;
+    } catch(error: any){
+        throw{status: error?.status || 500, message: error?.message || error}
+    }
+}
+
+const getCogerTurnoPorIdProfesor = (idProfesor: number) => {
+    
+    try {
+        const cogerTurnoRapido = DB.JuegoCogerTurnoRapido.filter((juegoCogerTurnoRapido: JuegoCogerTurnoRapido) => juegoCogerTurnoRapido.profesorId === idProfesor);
+        if (!cogerTurnoRapido) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar el juegoCogerTurnoRapido con idProfesor ${idProfesor} `
+            }
+        }
+        return cogerTurnoRapido;
+    } catch (error: any){
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+
+}
+
+const createJuegoCogerTurnoRapido = (newJuegoRapido: JuegoCogerTurnoRapido) => {
+    try {
+        DB.JuegoCogerTurnoRapido.push(newJuegoRapido);
+        utils.saveToDatabase(DB);
+        return newJuegoRapido;
+    } catch(error: any) {
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+   
+}
+
+const deleteCogerTurnoRapidoPorId = (id: string) => {
+    try {
+        const indexParaEliminar = DB.JuegoCogerTurnoRapido.findIndex((juegoCogerTurnoRapido: JuegoCogerTurnoRapido) => juegoCogerTurnoRapido.id === id);
+        if (indexParaEliminar === -1) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar juegoCuestionarioRapido con el id '${id}'`
+            }
+        }
+        const deleteCogerTurnoRapidoPorId = DB.JuegoCogerTurnoRapido[indexParaEliminar];
+        DB.JuegoCogerTurnoRapido.splice(indexParaEliminar, 1);
+        utils.saveToDatabase(DB);
+
+        return deleteCogerTurnoRapidoPorId;
+
+    } catch (error: any) {
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+   
+}
+
+const updateJuegoCogerTurnoRapido = (changesJuegoCogerTurnoRapido: JuegoCogerTurnoRapido, id?: string) => {
+    try {
+        const indexParaActualizar = DB.JuegoCogerTurnoRapido.findIndex((juegoCogerTurnoRapido: JuegoCogerTurnoRapido) => juegoCogerTurnoRapido.id === id);
+        if (indexParaActualizar === -1) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar juegoCogerTurnoRapido con el id '${id}'`
+            };
+        }
+        const updatedJuegoCogerTurnoRapido = {
+            ...DB.JuegoCogerTurnoRapido[indexParaActualizar],
+            ...changesJuegoCogerTurnoRapido,
+        };
+        DB.JuegoCogerTurnoRapido[indexParaActualizar] = updatedJuegoCogerTurnoRapido;
+        utils.saveToDatabase(DB);
+    
+        return updatedJuegoCogerTurnoRapido;
+    } catch (error: any) {
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+}
+
+export { getAllCogerTurnoRapido, createJuegoCogerTurnoRapido, getCogerTurnoPorIdProfesor, getCogerTurnoRapidoPorId, deleteCogerTurnoRapidoPorId, updateJuegoCogerTurnoRapido }
