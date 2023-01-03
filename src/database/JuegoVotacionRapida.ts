4fa649db00d6dca46387dcbcaf41e1ffa2756e5e
Pablo <kevin.alca@gmail.com>
2023-01-03 21:25:46 +0100

    CRUD votacion rapida

---
+++ src/database/JuegoVotacionRapida.ts
@@ -0,0 +1,95 @@
+import { JuegoVotacionRapida } from "../interfaces/juegoVotacionRapidaInterface";
+const DB = require("./db.json");
+import * as utils from "../utils/utilsDatabase";
+
+const getAllVotacionRapida = () => {
+    return DB.JuegoVotacionRapida;
+}
+
+const getVotacionRapidaPorId = (id: string) =>{
+    try {
+        const votacionRapida = DB.JuegoVotacionRapida.find((juegoVotacionRapida:any)=> juegoVotacionRapida === id);
+        if(!votacionRapida){
+            throw{
+                status: 400, 
+                message: `No se puede encontrar el juegoVotacionRapido con id ${id} `
+            }
+        }
+        return votacionRapida;
+    } catch(error: any) {
+        throw{status: error?.status || 500, message: error?.message || error};
+    }
+
+}
+
+const getVotacionRapidaPorIdProfesor = (idProfesor: number) => {
+    
+    try {
+        const votacionRapida = DB.juegoVotacionRapida.filter((juegoVotacionRapida: JuegoVotacionRapida) => juegoVotacionRapida.profesorId === idProfesor);
+        if (!votacionRapida) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar el juegoCuestionarioRapido con idProfesor ${idProfesor} `
+            }
+        }
+        return votacionRapida;
+    } catch (error: any){
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+}
+
+const createJuegoVotacionRapida = (newJuegoRapido: JuegoVotacionRapida) => {
+    try {
+        DB.JuegoVotacionRapida.push(newJuegoRapido);
+        utils.saveToDatabase(DB);
+        return newJuegoRapido;
+    } catch(error: any) {
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+}
+
+const deleteJuegoVotacionRapidaPorId = (id: string) => {
+    try {
+        const indexParaEliminar = DB.JuegoVotacionRapida.findIndex((juegoVotacionRapida: JuegoVotacionRapida) => juegoVotacionRapida.id === id);
+        if (indexParaEliminar === -1) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar juegoVotacionRapida con el id '${id}'`
+            }
+        }
+        const deletedVotacionRapida = DB.JuegoVotacionRapida[indexParaEliminar];
+        DB.JuegoVotacionRapida.splice(indexParaEliminar, 1);
+        utils.saveToDatabase(DB);
+
+        return deletedVotacionRapida;
+
+    } catch (error: any) {
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+}
+
+
+const updateJuegoVotacionRapida = (changesJuegoVotacionRapida: JuegoVotacionRapida, id?: string) => {
+    try {
+        const indexParaActualizar = DB.JuegoVotacionRapida.findIndex((juegoVotacionRapida: JuegoVotacionRapida) => juegoVotacionRapida.id === id);
+        if (indexParaActualizar === -1) {
+            throw {
+                status: 400,
+                message: `No se puede encontrar juegoVotacionRapida con el id '${id}'`
+            };
+        }
+        const updatedJuegoVotacionRapida = {
+            ...DB.JuegoVotacionRapida[indexParaActualizar],
+            ...changesJuegoVotacionRapida,
+        };
+        DB.JuegoVotacionRapida[indexParaActualizar] = updatedJuegoVotacionRapida;
+        utils.saveToDatabase(DB);
+    
+        return updatedJuegoVotacionRapida;
+    } catch (error: any) {
+        throw {status: error?.status || 500, message: error?.message || error  };
+    }
+
+}
+
+export{getAllVotacionRapida, createJuegoVotacionRapida, getVotacionRapidaPorId, getVotacionRapidaPorIdProfesor, deleteJuegoVotacionRapidaPorId, updateJuegoVotacionRapida}
---
+++ src/services/juegoVotacionRapidaService.ts
@@ -0,0 +1,78 @@
+import * as juegoVotacionRapida from "../database/JuegoVotacionRapida";
+import { v4 as uuidv4 } from 'uuid';
+import { JuegoVotacionRapida } from "../interfaces/juegoVotacionRapidaInterface";
+
+const getAllVotacionRapida = () => {
+    const allVotacionRapida = juegoVotacionRapida.getAllVotacionRapida()
+    return allVotacionRapida;
+}
+
+const getJuegoVotacionRapidaPorId = (id: string) => {
+    try {
+        const juegoVotacion = juegoVotacionRapida.getVotacionRapidaPorId(id);
+        return juegoVotacion;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getJuegoVotacionRapidaPorIdProfesor = (idProfesor: number) => {
+    try {
+        const juegoVotacion = juegoVotacionRapida.getVotacionRapidaPorIdProfesor(idProfesor);
+        return juegoVotacion;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const createJuegoVotacionRapida = (newJuegoVotacionRapida: JuegoVotacionRapida) => {
+
+    if (!newJuegoVotacionRapida.NombreJuego ||
+        !newJuegoVotacionRapida.Tipo ||
+        !newJuegoVotacionRapida.ModoReparto ||
+        !newJuegoVotacionRapida.Clave ||
+        !newJuegoVotacionRapida.profesorId ||
+        !newJuegoVotacionRapida.Respuestas ||
+        !newJuegoVotacionRapida.Conceptos) {
+        throw {
+            status: 400,
+            message: `Uno o mas parametros del juegoVotacionRapido faltan {NombreJuego, Tipo..} `
+        }
+    }
+
+    newJuegoVotacionRapida.id = uuidv4();
+
+    try {
+        const createdJuegoVotacionRapidA = juegoVotacionRapida.createJuegoVotacionRapida(newJuegoVotacionRapida);
+        return createdJuegoVotacionRapidA;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const deleteJuegoVotacionRapidaPorId = (id: string) => {
+    try {
+        const deleteJuegoVotacionRapida = juegoVotacionRapida.deleteJuegoVotacionRapidaPorId(id);
+        return deleteJuegoVotacionRapida;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const updateJuegoVotacionRapida = (updateJuegoVotacionRapida: JuegoVotacionRapida) => {
+    try {
+        const juegoVotacion = juegoVotacionRapida.updateJuegoVotacionRapida(updateJuegoVotacionRapida, updateJuegoVotacionRapida.id);//CAMBIAR
+        return juegoVotacion;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export {
+    getAllVotacionRapida,
+    createJuegoVotacionRapida,
+    getJuegoVotacionRapidaPorId,
+    getJuegoVotacionRapidaPorIdProfesor,
+    deleteJuegoVotacionRapidaPorId,
+    updateJuegoVotacionRapida
+};
